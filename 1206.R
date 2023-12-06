675257c7a6d6393fb4ab613686db840e9f141c00
b908110065 <126707860+b908110065@users.noreply.github.com>
2023-12-06 13:37:05 +0800

    Add files via upload

---
+++ 1206.R
@@ -0,0 +1,8 @@
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+if(oneComingStudentMajor == "Economics"){
+  print("Please go to room 201.")
+}
+
+if(oneComingStudentMajor == "Sociology"){
+  print("Please go to room 301.")
+}
